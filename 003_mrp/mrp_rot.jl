b6cc175011c3b4b844000ed20c20bfd9f1ac0ab3
hyrodium <hyrodium@gmail.com>
2021-05-15 02:31:29 +0900

    add MRP y-axis and z-axis

--- 003_mrp/mrp_rot.jl
+++ 003_mrp/mrp_rot.jl
@@ -47,5 +47,38 @@ for t in ts
     lines *= line * "\n"
 end
 print(lines)
-
 write("003_mrp/02_mrp_x/rot_x.txt",lines)
+
+# Generate MRP around y-axis
+ts = range(0,stop=2π,length=61)[1:end-1]
+lines = ""
+for t in ts
+    R_x = RotY(t)
+    p = insideball(Rotations.params(MRP(R_x)))
+    R_v = vec(Matrix(R_x))
+    line = ""
+    line *= "<$(string(R_v[1:3])[2:end-1])>,"
+    line *= "<$(string(R_v[4:6])[2:end-1])>,"
+    line *= "<$(string(R_v[7:9])[2:end-1])>,"
+    line *= "<$(string(p)[2:end-1])>,"
+    lines *= line * "\n"
+end
+print(lines)
+write("003_mrp/03_mrp_y/rot_y.txt",lines)
+
+# Generate MRP around z-axis
+ts = range(0,stop=2π,length=61)[1:end-1]
+lines = ""
+for t in ts
+    R_x = RotZ(t)
+    p = insideball(Rotations.params(MRP(R_x)))
+    R_v = vec(Matrix(R_x))
+    line = ""
+    line *= "<$(string(R_v[1:3])[2:end-1])>,"
+    line *= "<$(string(R_v[4:6])[2:end-1])>,"
+    line *= "<$(string(R_v[7:9])[2:end-1])>,"
+    line *= "<$(string(p)[2:end-1])>,"
+    lines *= line * "\n"
+end
+print(lines)
+write("003_mrp/04_mrp_z/rot_z.txt",lines)
